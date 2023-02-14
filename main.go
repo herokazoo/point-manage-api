f7599ae62d3e02281208e82e44cdd47ad3f76fbf
herokazoo <hrkzo38@gmail.com>
2023-02-15 00:45:59 +0900

    refactor main.go

--- main.go
+++ main.go
@@ -5,14 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
 	"github.com/herokazoo/point-manage-api/config"
-	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -22,18 +17,7 @@ func main() {
 	}
 }
 
-/*
-関数の外部からサーバーのプロセスを中断可能にする
-動的にポート番号を変更可能する->環境変数から設定をロードする
-
-	1.*http.Server.ListenAndServeメソッドを実行してHTTPリクエストを受け付ける
-	2.引数で渡されたcontext.Contextを通じて処理の中断命令を検知した時は
-	  *http.Server.ShutdownメソッドでHTTPサーバーの機能を終了する
-	3.run関数の戻り値として*http.Server.ListenAndServeメソッドの戻り値のエラーを返す
-*/
 func run(ctx context.Context) error {
-	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
-	defer stop()
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -44,32 +28,7 @@ func run(ctx context.Context) error {
 	}
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
-	s := &http.Server{
-		// 引数で受け取ったnet.Listenerを利用するので、Addrフィールドは指定しない
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// コマンドラインで中断実験用
-			time.Sleep(5 * time.Second)
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	}
-	eg, ctx := errgroup.WithContext(ctx)
-	// 別ゴルーチンでHTTPサーバーを起動する
-	eg.Go(func() error {
-		// ListenAndServeメソッドではなく、Serveメソッドに変更する
-		// http.ErrServerClosedは
-		// http.Server.Shutdown()が正常に終了したことを示すので異常ではない
-		if err := s.Serve(l); err != nil &&
-			err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
-	// チャネルからの通知（終了通知）を待機する
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
-	// Goメソッドで起動した別ゴルーチンの終了を待つ
-	return eg.Wait()
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
